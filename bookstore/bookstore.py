1c8af82295d71a5e1645a6f45e9c2c6d3b641d56
Thomas McCabe <tmccabe@stevens.edu>
2017-04-07 02:25:31 +0000

    Thomas McCabe and Chris Holder OOP Dictionary

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,53 @@
 class Bookstore(object):
-    pass
+    
+    def __init__(self, name):
+        self.name = name
+        self.books = []
 
-
+    def get_books(self):
+        return self.books
+    
+    def add_book(self, book):
+        self.books.append(book)
+    
+    def search_books(self, title=None, author=None):
+        """
+        booklist = []
+        if title:
+            for book in self.books:
+                if title.lower() in book.title.lower():
+                    booklist.append(book)
+        if author:
+            for book in self.books:
+                if author == book.author:
+                    booklist.append(book)
+        return booklist
+        """
+        
+        booklist = []
+        for book in self.books:
+            if (author == book.author) or (title is not None and title.lower() in book.title.lower()):
+                booklist.append(book)
+        return booklist
+            
 class Author(object):
-    pass
-
+    
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.books = []
+    
+    def add_book(self, book):
+        self.books.append(book)
+    
+    def get_books(self):
+        return self.books
 
 class Book(object):
-    pass
+    
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.author.add_book(self)
+        # (or) self.author.books.append(self)
+    
